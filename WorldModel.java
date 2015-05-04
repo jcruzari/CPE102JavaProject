fb701188709e27fab3cb5d3cdd36a46282a5ca34
Jose Cruz Arinaga <jcruzari@calpoly.edu>
2015-05-04 16:22:33 -0700

    Added methods to the WorldModel class

--- WorldModel.java
+++ WorldModel.java
@@ -22,4 +22,44 @@ public class WorldModel{
          pt.yCoord() >= 0 && pt.yCoord() < this.numRows);
    }
 
+   public boolean isOccupied(Point pt){
+      return (this.withinBounds(pt) && this.occupancy[pt.yCoord()]
+         [pt.xCoord()] != null);
+   }
+
+   public void addEntity(Entity e){
+      Point pt = e.getPosition();
+      Entity oldEntity;
+      if(this.withinBounds(pt)){
+         oldEntity = this.occupancy[pt.yCoord()][pt.xCoord()];
+         this.occupancy[pt.yCoord()][pt.xCoord()] = e;
+         this.entities.add(e);
+      }
+   }
+
+   //Static methods
+
+   public static double distanceSq(Point p1, Point p2){
+      return Math.sqrt(Math.pow(p1.xCoord() - p2.xCoord(), 2) + 
+         Math.pow(p1.yCoord() - p2.yCoord(), 2));
+   }   
+
+   /*public static Entity nearestEntity(List<Entity> entityDists){
+      List<Entity> pair = new LinkedList<Entity>();
+      List<Entity> other = new LinkedList<Entity>();
+      Entity nearest;
+      if(entityDists.size() > 0){
+         pair.add(entityDists(0));
+         for (other : entityDists){
+            if(other(1) < pair(1)){
+               pair = other;
+            }
+         }
+         nearest = pair(0);
+      }
+      else{
+         nearest = null;
+      }
+      return nearest;
+   }*/
 }
