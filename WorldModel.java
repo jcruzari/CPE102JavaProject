d2b45b8e9cd079644af5ad270cc7af663c95bc45
Jose Cruz Arinaga <jcruzari@calpoly.edu>
2015-05-04 17:27:23 -0700

    Added more methods to WorldModel, this version does not compile

--- WorldModel.java
+++ WorldModel.java
@@ -37,6 +37,59 @@ public class WorldModel{
       }
    }
 
+   public List<Point> moveEntity(Entity e, Point pt){
+      List<Point> tiles = new LinkedList<Point>();
+      if(this.withinBounds(pt)){
+         Point oldPt = e.getPosition();
+         this.occupancy[pt.yCoord()][pt.xCoord()] = null;
+         tiles.add(oldPt);
+         this.occupancy[pt.yCoord()][pt.xCoord()] = e;
+         tiles.add(pt);
+         e.setPosition(pt);
+      }
+      return tiles;
+   }
+
+   public void removeEntityAt(Point pt){
+      if(this.withinBounds(pt) && 
+         this.occupancy[pt.yCoord()][pt.xCoord()] != null){
+         Entity e = this.occupancy[pt.yCoord()][pt.xCoord()];
+         e.setPosition(new Point(-1, 1));
+         this.entities.remove(e);
+         this.occupancy[pt.yCoord()][pt.xCoord()] = null;
+      }
+   }
+
+   public void removeEntity(Entity e){
+      this.removeEntityAt(e.getPosition());
+   }
+
+   public Background getBackground(Point pt){
+      Background b = null;
+      if(this.withinBounds(pt)){
+         b = this.backgrounds[pt.yCoord()][pt.xCoord()];
+      }
+      return b;
+   }
+
+   public void setBackground(Point pt, Background b){
+      if(this.withinBounds){
+         this.backgrounds[pt.yCoord()][pt.xCoord()] = b;
+      }
+   }
+
+   public Entity getTileOccupant(Point pt){
+      
+   }
+
+   public List<Entity> getEntities(){
+      return this.entities;
+   }
+
+   public Point nextPosition(Point entityPt, Point destPt){
+      int horiz = sign(destPt.xCoord() - entityPt.xCoord());
+   }
+
    //Static methods
 
    public static double distanceSq(Point p1, Point p2){
@@ -44,7 +97,7 @@ public class WorldModel{
          Math.pow(p1.yCoord() - p2.yCoord(), 2));
    }   
 
-   /*public static Entity nearestEntity(List<Entity> entityDists){
+   /*public static Entity nearestEntity(List<Entity> entityDists, Point pt){
       List<Entity> pair = new LinkedList<Entity>();
       List<Entity> other = new LinkedList<Entity>();
       Entity nearest;
@@ -62,4 +115,21 @@ public class WorldModel{
       }
       return nearest;
    }*/
+
+   public static int sign(int x){
+      if(x < 0){
+         return -1
+      }
+      else if(x > 0){
+         return 1;
+      }
+      return 0;
+   }
+
+   public static boolean adjacent(Point pt1, Point pt2){
+      return ((pt1.xCoord() == pt.xCoord() && 
+         Math.abs(pt1.yCoord() - pt2.yCoord()) == 1) ||
+            (pt1.yCoord() == pt2.yCoord() && Math.abs(pt1.xCoord() - 
+               pt2.xCoord()) == 1));
+   }
 }
